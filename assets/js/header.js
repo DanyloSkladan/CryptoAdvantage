760cf75a7a5f0965503e8b5e453288c3524e697a
Danylo Skladan <dskladannnnn09082001@gmail.com>
2024-06-21 04:17:29 +0300

    mobile__btn

--- assets/js/header.js
+++
@@ -1,7 +0,0 @@
-const mobileBtn = document.querySelector('.header__mobile-btn')
-const mobileMenu = document.querySelector('.header__menu-box')
-
-mobileBtn.addEventListener('click', () => {
-    mobileBtn.classList.toggle('active')
-    mobileMenu.classList.toggle('active')
-})
